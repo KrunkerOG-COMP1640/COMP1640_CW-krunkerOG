2a2a73da3cdf0dbcca3acfbf3cccad37a8c6c01e
Harinash <62490198+Harinash@users.noreply.github.com>
2023-03-09 17:32:56 +0800

    admin delete user

---
+++ DeleteUser_admin.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+require("krunkerideaconn.php");
+
+if(!isset($_SESSION['role'])){
+  header("Location: index.php");
+  exit;
+  
+}
+else{
+  if($_SESSION['role'] != "Admin"){ //staff cannot access admin page
+      header("Location: index.php");
+      // exit;
+  }
+}
+$id = $_GET['id'];
+
+$sql = "DELETE FROM `idea_tbl` WHERE IdeaId =$id";
+$result =mysqli_query($dbconn, $sql);
+
+  if($result){
+    header("Location: ManageIdea_admin.php?msg=User deleted successfully");
+  }
+  else{
+    echo "Failed: " .mysqli_error($dbconn);
+  }
+
+?>
