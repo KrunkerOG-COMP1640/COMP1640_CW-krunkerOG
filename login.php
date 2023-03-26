c84594deae49f983c3d63eb03120f10c85db3e2c
KhiewJiaEr <91199888+KhiewJiaEr@users.noreply.github.com>
2023-03-26 19:51:52 +0800

    login validation

--- login.php
+++ login.php
@@ -28,57 +28,56 @@ if(isset($_POST["userlogin"])){
     $myemail = $_POST["useremail"];
     $mypassword = $_POST["userpassword"];
     $sql = "SELECT * FROM user_tbl WHERE UserEmail = '$myemail' AND UserPassword = '$mypassword'";
-    $checkaccount = mysqli_query($dbconn,$sql );
-    
-    
+    $checkaccount = mysqli_query($dbconn,$sql);
+
     if(mysqli_num_rows($checkaccount) > 0){
         $userrow = mysqli_fetch_array($checkaccount);
     
-    if(!preg_match('/^[a-zA-Z0-9_@.!]+$/', $myemail)) {
+        if(!preg_match('/^[a-zA-Z0-9_@.!]+$/', $myemail)) {
 
-        header("Location: login.php");
+            header("Location: login.php");
 
-        echo '<script>';
-        echo 'alert("Do Not Insert Special Character")';
-        echo '</script>';
-    }
-    else{
-        if(is_array($userrow)) {
-            $_SESSION["userid"] = $userrow["UserId"];
-            $_SESSION["username"] = $userrow["Username"];
-            $_SESSION["useremail"] = $userrow["UserEmail"];
-            $_SESSION["userpassword"] = $userrow["UserPassword"];
-            $_SESSION["role"] = $userrow["UserRoleName"];
-            $_SESSION["useraddress"] = $userrow["UserAddress"];
-            $_SESSION["usercontactno"] = $userrow["UserContactNo"];
-
+            echo '<script>';
+            echo 'alert("Do Not Insert Special Character")';
+            echo '</script>';
         }
         else{
-            header("Location: login.php");
-        }
-        if(isset($_SESSION["username"]) 
-        && isset($_SESSION["userid"]) 
-        && isset($_SESSION["role"])) {
-            if($_SESSION["role"] == "Admin") {
-                header("Location: index_admin.php");
-                exit;
+            if(is_array($userrow)) {
+                $_SESSION["userid"] = $userrow["UserId"];
+                $_SESSION["username"] = $userrow["Username"];
+                $_SESSION["useremail"] = $userrow["UserEmail"];
+                $_SESSION["userpassword"] = $userrow["UserPassword"];
+                $_SESSION["role"] = $userrow["UserRoleName"];
+                $_SESSION["useraddress"] = $userrow["UserAddress"];
+                $_SESSION["usercontactno"] = $userrow["UserContactNo"];
+
             }
-            else if($_SESSION["role"] == "Staff") {
-                header("Location: index.php");
-                exit;
+            else{
+                header("Location: login.php");
             }
-            else if($_SESSION["role"] == "QA Manager") {
-                header("Location: index_manager.php");
-                exit;
+            if(isset($_SESSION["username"]) 
+            && isset($_SESSION["userid"]) 
+            && isset($_SESSION["role"])) {
+                if($_SESSION["role"] == "Admin") {
+                    header("Location: index_admin.php");
+                    exit;
+                }
+                else if($_SESSION["role"] == "Staff") {
+                    header("Location: index.php");
+                    exit;
+                }
+                else if($_SESSION["role"] == "QA Manager") {
+                    header("Location: index_manager.php");
+                    exit;
+                }
+                else if($_SESSION["role"] == "QA Manager") {
+                    header("Location: index_coordinator.php");
+                    exit;
+                }
+                
             }
-            else if($_SESSION["role"] == "QA Manager") {
-                header("Location: index_coordinator.php");
-                exit;
-            }
-            
         }
     }
-}
 }
 ?>
 
@@ -106,8 +105,6 @@ if(isset($_POST["userlogin"])){
         <form action="" method="post">
             <div class="input-field">
                 <input type="email" class="input" placeholder="Email"  name="useremail" id="">
-
-
                 <i class='bx bx-user' ></i>
             </div>
 
@@ -115,6 +112,14 @@ if(isset($_POST["userlogin"])){
                 <input type="password" class="input" placeholder="Password" name="userpassword" id="">
                 <i class='bx bx-lock-alt'></i>
             </div>
+
+            <?php
+                if(!empty($_POST["useremail"]) && !empty($_POST["userpassword"])){
+                    echo'<div class="input-field" style="color:Red;">';
+                        echo'<p>*Incorrect email address or password.</p>';
+                    echo'</div>';
+                }
+            ?>
 
             <div class="input-field">
                 <input type="submit" class="submit" value="Login" name="userlogin" id=""><br>  
