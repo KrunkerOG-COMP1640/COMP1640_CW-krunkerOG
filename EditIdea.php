afd405a3b1a1ad027afedf0ca94481b65e2228ec
Harinash <62490198+Harinash@users.noreply.github.com>
2023-04-08 16:17:16 +0800

    redirect issue

--- EditIdea.php
+++ EditIdea.php
@@ -2,6 +2,10 @@
 date_default_timezone_set('Asia/Kuala_Lumpur');
 session_start();
 
+if (!isset($_SESSION["username"]) && !isset($_SESSION["userid"])) {
+  header("Location: login.php"); // Redirect to login page if not logged in
+  exit;
+}
 
 $dbconn = mysqli_connect("localhost", "root", "", "krunkerideadb");
 //determine current page
