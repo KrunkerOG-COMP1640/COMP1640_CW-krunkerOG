3460912f23add42d1ffc03be5a93bd985b9cbfba
Faiz Rahman <52125715+FaizRahman01@users.noreply.github.com>
2023-04-14 11:22:24 +0800

    -

--- ManageCategory_manager.php
+++ ManageCategory_manager.php
@@ -226,7 +226,7 @@ $start= ($page-1)*$rows_per_page;
                       foreach($query_no as $row){
                         ?>
                         <tr>
-                        <td><?php echo htmlentities($row['CategoryTitle'])?></td>
+                        <td><?php echo $row['CategoryTitle']?></td>
                         <td><?php echo $row['DateCreated']?></td>
                         <td><a href="EditCategory_manager.php?id=<?php echo $row['CategoryId'] ?>"  class="btn btn-success">Edit</a></td>
                         <td><a href="DeleteCategory_manager.php?id=<?php echo $row['CategoryId'] ?>" class="btn btn-danger">Delete</a></td>
